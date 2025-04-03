ae7fed0bfb25bea126f918a682eeaa98e15c1879
tomomi ueno <chimdon.0410@gmail.com>
2025-04-03 19:59:41 +0900

    Q2を回答しました。

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
+// Q1
+
+
 let nickname = 'ともみ';
 let age = '27歳';
 
 let selfintrodaction = '私のニックネームは' + nickname + 'です。 年齢は' + age + 'です。';
 console.log(selfintrodaction) ;
 
+//Q2
+
+
+let language1 = 'JavaScript';
+let language2 = 'PHP';
+let language3 = 'Ruby';
+let language4 = 'Python';
+let language5 = 'Go' ;
+
+
+let templateliterals = `私の好きな言語は${language1}です。次は${language3}を勉強してみたいです。`;
+console.log(templateliterals);
