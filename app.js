a1719446b230f939a2221e36aa14899c66fe70f6
tomomi ueno <chimdon.0410@gmail.com>
2025-04-07 20:30:28 +0900

    mainブランチの作成

--- app.js
+++ app.js
@@ -4,8 +4,12 @@
 let nickname = 'ともみ';
 let age = '27歳';
 
-let selfintrodaction = '私のニックネームは' + nickname + 'です。 年齢は' + age + 'です。';
-console.log(selfintrodaction) ;
+let selfintrodaction =
+  '私のニックネームは' + nickname + 'です。 年齢は' + age + 'です。';
+
+console.log(selfintrodaction);
+
+
 
 //Q2
 
@@ -14,19 +18,9 @@ let language1 = 'JavaScript';
 let language2 = 'PHP';
 let language3 = 'Ruby';
 let language4 = 'Python';
-let language5 = 'Go' ;
+let language5 = 'Go';
 
 
 let templateliterals = `私の好きな言語は${language1}です。次は${language3}を勉強してみたいです。`;
+
 console.log(templateliterals);
-
-//Q3
-
-let user = {
-  name: 'John',
-  age: 26,
-  bloodType: 'A',
-  favorite: 'card',
-};
-
-console.log(user.age);
