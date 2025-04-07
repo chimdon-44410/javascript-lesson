3bd4ba1e7a293a6b4a471f2fb08b26e6ab4b09e2
tomomi ueno <chimdon.0410@gmail.com>
2025-04-07 20:32:43 +0900

    全部回答しました。

--- app.js
+++ app.js
@@ -4,8 +4,12 @@
 let nickname = 'ともみ';
 let age = '27歳';
 
-let selfintrodaction = '私のニックネームは' + nickname + 'です。 年齢は' + age + 'です。';
-console.log(selfintrodaction) ;
+let selfintrodaction =
+  '私のニックネームは' + nickname + 'です。 年齢は' + age + 'です。';
+
+console.log(selfintrodaction);
+
+
 
 //Q2
 
@@ -14,11 +18,14 @@ let language1 = 'JavaScript';
 let language2 = 'PHP';
 let language3 = 'Ruby';
 let language4 = 'Python';
-let language5 = 'Go' ;
+let language5 = 'Go';
 
 
 let templateliterals = `私の好きな言語は${language1}です。次は${language3}を勉強してみたいです。`;
+
 console.log(templateliterals);
+
+
 
 //Q3
 
@@ -30,3 +37,97 @@ let user = {
 };
 
 console.log(user.age);
+
+//Q4
+
+let playerList = [
+  { name: 'John', age: 26, favorites: ['Card Game', 'Basket Ball', 'Programming'] },
+  { name: 'Bob', age: 33, favorites: ['Tinder', 'The Legend of Zelda'] },
+  { name: 'Michael', age: 22, favorites: ['Football', 'Smash Bros.'] },
+];
+
+let bob = playerList[1]
+console.log(bob.favorites[1]);
+
+
+
+//Q5
+let john = (playerList[0].age);
+let bob_ = (playerList[1].age);
+let michael = (playerList[2].age);
+
+let eachAge = [john, bob_, michael];
+
+let sum = 0;
+
+for (let i = 0; i < eachAge.length; i++) {
+  sum += eachAge[i];
+}
+console.log(sum / eachAge.length);
+
+//Q6
+function Hello() {
+  console.log('sayHello');
+}
+Hello();
+
+let sayWorld = 'World';
+console.log(sayWorld);
+
+//Q7
+
+let user = {
+  name: 'John',
+  age: 26,
+  bloodType: 'A',
+  favorite: 'card',
+  birthday: '2000-09-27',
+  sayHello: 'Hello!'
+};
+
+console.log(user.sayHello);
+
+
+//Q8
+
+let calc = {
+  add: function (x, y) {
+    return (x + y);
+  },
+
+  subtract: function (x, y) {
+    return (x - y);
+  },
+
+  multiply: function (x, y) {
+    return (x * y);
+  },
+
+  divide: function (x, y) {
+    return (x / y);
+  }
+};
+
+console.log(calc.add(3, 4));
+console.log(calc.subtract(30, 20));
+console.log(calc.multiply(7, 7));
+console.log(calc.divide(25, 5));
+
+//Q9
+function remainder(x, y) {
+  return (x % y);
+}
+
+let x = 5;
+let y = 3;
+let z = remainder(x, y);
+
+console.log( x + ' を ' +  y  +  ' で割った余りは' +  z  + 'です。');
+
+//Q10
+// スコープとは変数の有効範囲のことであり、
+// function foo() {
+//   let x = 1;
+// }
+// console.log(x);
+// のxは{}の範囲内でないと変数が参照できないため範囲外にあるconsole.log(x);ではエラーが出てしまう。
